64cf90ff58411156f5b49bdaefa9724d4b5b61d3
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-05-30 12:53:07 +0000

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let sales = 800596;
+let course = "TypeScript Fundamentals";
+let isPublished = true;
+let level = 200;
+let center;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let sales: number = 800_596;
+let course: string = "TypeScript Fundamentals";
+let isPublished: boolean = true;
+let level =200;
+let center;
